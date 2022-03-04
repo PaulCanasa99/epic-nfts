60de42efe9bcbcdb34a1629290fbe92619533755
PaulCanasa99 <paul.canasa@pucp.edu.pe>
2022-03-03 20:00:31 -0500

    NFT Project

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-etherscan");
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  }
+};
